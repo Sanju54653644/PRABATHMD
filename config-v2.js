f23b3d82e053ad38ecba8a00671774c3bf3d7150
Sanjoy mahato <124846542+Sanju54653644@users.noreply.github.com>
2024-11-03 17:01:46 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "918927384393"
+const SESSION_ID = "PRABATH-MD~6XBAWbqA#S6jKs1eq04J4RP5sE0uSPq-abhZxeWCNpXLv6afsmOY"
 
 
 module.exports = {
